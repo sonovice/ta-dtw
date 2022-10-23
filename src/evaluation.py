9198ccf0565722aea2459ad94d014bc02688ce4d
Simon Waloschek <waloschek@pm.me>
2022-10-23 21:19:04 +0200

    Finalized evaluation

--- src/evaluation.py
+++ src/evaluation.py
@@ -52,66 +52,67 @@ def evaluate(rec_id, has_drift: bool, feature_type: str, is_transposition_aware:
     return results
 
 
-def experiment_worker(args):
+def configuration_worker(args):
     return evaluate(*args), args
 
 
-def run_experiments():
+def run_configurations():
     results_path = Path(f'../experiments/')
     results_path.mkdir(parents=True, exist_ok=True)
     rec_ids = list(dataset.features.keys())
 
-    experiments_all = []
+    configurations_all = []
 
-    experiments_metrics = list(itertools.product(
+    configurations_metrics = list(itertools.product(
         rec_ids,
         [False],  # has_drift
-        ['chroma'],  # feature_type
+        ['pcp'],  # feature_type
         [False],  # is_transposition_aware
         ['cosine', 'euclidean', 'cityblock'],  # metric
         [1]  # transposition_penalty
     ))
-    experiments_all.extend(experiments_metrics)
+    configurations_all.extend(configurations_metrics)
 
-    experiments_transposition_penalty = list(itertools.product(
+    configurations_transposition_penalty = list(itertools.product(
         rec_ids,
         [False, True],  # has_drift
         ['pcp'],  # feature_type
         [True],  # is_transposition_aware
-        ['cosine'],  # metric
-        [1] + list(range(10, 101, 10)) + list(range(5, 30))  + list(np.arange(7, 12.5, 0.5).tolist())  # transposition_penalty
+        ['euclidean'],  # metric
+        [1, 10, 20] + list(range(1, 11)) + [6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5]
+        # transposition_penalty
     ))
-    experiments_all.extend(experiments_transposition_penalty)
+    configurations_all.extend(configurations_transposition_penalty)
 
-    # experiments_fixed_tp = list(itertools.product(
-    #     rec_ids,
-    #     [False, True],  # has_drift
-    #     ['chroma', 'pcp'],  # feature_type
-    #     [False, True],  # is_transposition_aware
-    #     ['cosine'],  # metric
-    #     [24]  # transposition_penalty
-    # ))
-    # experiments_all.extend(experiments_fixed_tp)
+    configurations_fixed_tp = list(itertools.product(
+        rec_ids,
+        [False, True],  # has_drift
+        ['chroma', 'pcp'],  # feature_type
+        [False, True],  # is_transposition_aware
+        ['euclidean'],  # metric
+        [6.7, 8.0, 8.1]  # transposition_penalty
+    ))
+    configurations_all.extend(configurations_fixed_tp)
 
-    # Remove already saved experiments
-    experiments = []
-    for exp in experiments_all:
-        rec_id, has_drift, feature_type, is_transposition_aware, metric, transposition_penalty = exp
+    # Remove already saved configurations
+    configurations = []
+    for config in configurations_all:
+        rec_id, has_drift, feature_type, is_transposition_aware, metric, transposition_penalty = config
         file_name = f"{rec_id}_drift={has_drift}_feature={feature_type}_algo={'ta-dtw' if is_transposition_aware else 'dtw'}_metric={metric}_tp={transposition_penalty}.json"
         if not (results_path / file_name).exists():
-            experiments.append(exp)
+            configurations.append(config)
 
-    # Execute actual experiments and save each results per file
-    with mp.get_context('spawn').Pool(6) as pool:
-        for results, args in tqdm(pool.imap_unordered(experiment_worker, experiments), total=len(experiments)):
+    # Execute actual configurations and save each results per file
+    with mp.get_context('spawn').Pool(12) as pool:
+        for results, args in tqdm(pool.imap_unordered(configuration_worker, configurations), total=len(configurations)):
             rec_id, has_drift, feature_type, is_transposition_aware, metric, transposition_penalty = args
             file_name = f"{rec_id}_drift={has_drift}_feature={feature_type}_algo={'ta-dtw' if is_transposition_aware else 'dtw'}_metric={metric}_tp={transposition_penalty}.json"
             with open(results_path / file_name, 'w') as fp:
                 json.dump(results, fp)
 
 
-def index_from_experiments() -> list:
-    input_path = Path(f'../experiments/')
+def index_from_configurations() -> list:
+    input_path = Path(f'../configurations/')
     index = []
     for path in input_path.glob('*.json'):
         parts = str(path.stem).split('_')
@@ -129,11 +130,11 @@ def index_from_experiments() -> list:
 
 
 def viz_metrics():
-    index = index_from_experiments()
+    index = index_from_configurations()
     index = filter(
         lambda e: all([
             not e.has_drift,
-            e.feature_type == 'chroma',
+            e.feature_type == 'pcp',
             not e.is_transposition_aware,
             e.transposition_penalty == 1,
         ]),
@@ -167,14 +168,14 @@ def viz_metrics():
     plt.legend(loc='upper left')
 
 
-def viz_transposition_penalty():
-    index = index_from_experiments()
+def viz_transposition_penalty(metric):
+    index = index_from_configurations()
     index = filter(
         lambda e: all([
             e.feature_type == 'pcp',
             e.is_transposition_aware,
-            e.metric == 'cosine',
-            e.transposition_penalty <= 100 #or e.transposition_penalty == float('inf'),
+            e.metric == metric,
+            e.transposition_penalty <= 20,
         ]),
         index
     )
@@ -200,10 +201,10 @@ def viz_transposition_penalty():
     x = tps
     y_drift = [(num_matches_per_tp_drift[tp] / num_onsets_per_tp_drift[tp]) * 100 for tp in x]
     y_original = [(num_matches_per_tp_original[tp] / num_onsets_per_tp_original[tp]) * 100 for tp in x]
-    y_mixed = [(yd+yo)/2 for yd, yo in zip(y_drift, y_original)]
-    ax.plot(x, y_drift, ':.', label='With drift')
-    ax.plot(x, y_original, ':.', label='Without drift')
-    ax.plot(x, y_mixed, '--.', label='Mixed')
+    y_mixed = [(yd + yo) / 2 for yd, yo in zip(y_drift, y_original)]
+    ax.plot(x, y_drift, '--.', label='With drift')
+    ax.plot(x, y_original, '--.', label='Without drift')
+    ax.plot(x, y_mixed, ':.', label='Mixed')
     ax.set_title('Transposition penalty factor influence')
     ax.set_xlabel('Transposition penalty factor')
     ax.set_ylabel('Score in percent')
@@ -211,12 +212,12 @@ def viz_transposition_penalty():
     ax.legend()
 
 
-def viz_final_results():
-    index = index_from_experiments()
+def viz_final_results(tp, metric):
+    index = index_from_configurations()
     index = filter(
         lambda e: all([
-            e.transposition_penalty == 1,
-            e.metric == 'cosine'
+            e.transposition_penalty == tp,
+            e.metric == metric
         ]),
         index
     )
@@ -226,72 +227,52 @@ def viz_final_results():
         with open(entry.path, 'rb') as fp:
             data = Addict(orjson.loads(fp.read()))
             for interval in INTERVALS:
-                if entry.feature_type not in results[str(entry.has_drift)][str(entry.is_transposition_aware)][interval]:
+                if interval not in results[str(entry.has_drift)][str(entry.is_transposition_aware)][entry.feature_type]:
                     results[str(entry.has_drift)][str(entry.is_transposition_aware)][entry.feature_type][interval] = [0, 0]
                 results[str(entry.has_drift)][str(entry.is_transposition_aware)][entry.feature_type][interval][0] += data.num_onsets
                 results[str(entry.has_drift)][str(entry.is_transposition_aware)][entry.feature_type][interval][1] += data.matches_in_interval[str(interval)]
 
-
-    # Plot
     fig, axs = plt.subplots(1, 2, constrained_layout=True, sharex=True, sharey=True)
+    combinations = list(itertools.product(
+        ['False', 'True'],  # has drift
+        ['False', 'True'],  # is transposition aware
+        ['chroma', 'pcp'],  # feature type
+    ))
 
-    # Without drift
-    entry = results['False']['False']['chroma']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[0].plot(intervals, values, ':s', label='DTW, Chroma')
+    for has_drift, is_transposition_aware, feature_type in combinations:
+        entry = results[has_drift][is_transposition_aware][feature_type]
+        has_drift = has_drift == 'True'
+        is_transposition_aware = is_transposition_aware == 'True'
+        intervals = [f'≤{i}s' for i in entry.keys()]
+        values = [(matched / num) * 100 for num, matched in list(entry.values())]
 
-    entry = results['False']['False']['pcp']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[0].plot(intervals, values, ':o', label='DTW, PCP')
+        label = f"{'TA-DTW' if is_transposition_aware else 'DTW'}, {feature_type}"
+        style = f"{'--' if is_transposition_aware else ':'}{'s' if feature_type == 'chroma' else 'o'}"
+        axs[1 if has_drift else 0].plot(intervals, values, style, label=label)
 
-    entry = results['False']['True']['chroma']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[0].plot(intervals, values, '--s', label='TA-DTW, Chroma')
-
-    entry = results['False']['True']['pcp']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[0].plot(intervals, values, '--o', label='TA-DTW, PCP')
+        print()
+        print(label)
+        for i, v in zip(intervals, values):
+            print(f'{i:>6} - {v:.2f}%')
 
     axs[0].set_title('Without drift')
-    axs[0].set_ylim(0, 100)
+    axs[0].set_ylim(-10, 110)
     axs[0].legend(loc='lower right')
 
-    # With drift
-    entry = results['True']['False']['chroma']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[1].plot(intervals, values, ':s', label='DTW, Chroma')
-
-    entry = results['True']['False']['pcp']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[1].plot(intervals, values, ':o', label='DTW, PCP')
-
-    entry = results['True']['True']['chroma']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[1].plot(intervals, values, '--s', label='TA-DTW, Chroma')
-
-    entry = results['True']['True']['pcp']
-    intervals = [f'≤{i}s' for i in entry.keys()]
-    values = [(matched / num) * 100 for num, matched in list(entry.values())]
-    axs[1].plot(intervals, values, '--o', label='TA-DTW, PCP')
-
     axs[1].set_title('With drift')
-    axs[1].set_ylim(0, 100)
+    axs[1].set_ylim(-10, 110)
     axs[1].legend(loc='lower right')
+
+    fig.suptitle(f"Transposition penalty = {tp}, metric = {metric}")
 
 
 if __name__ == '__main__':
-    # run_experiments()
-    # viz_transposition_penalty()
-    # viz_metrics()
-    # viz_final_results()
+    run_configurations()
+
+    viz_metrics()
+    viz_transposition_penalty('euclidean')
+    # viz_final_results(6.7, metric='euclidean')  # best with drift
+    viz_final_results(8.0, metric='euclidean')  # best mixed
+    # viz_final_results(8.1, metric='euclidean')  # best without drift
 
     plt.show()
-
-
